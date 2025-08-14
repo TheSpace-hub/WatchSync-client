efbcb8ba6d4303bf99e8013ab48365d58ea93330
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-14 22:03:33 +0300

    Сделал поле адреса подключения

--- src/scenes/intro.py
+++ src/scenes/intro.py
@@ -10,7 +10,7 @@ from pygame import Vector2
 
 from src.scene import Scene
 
-from src.sprites import Text, Button, InBlockText
+from src.sprites import Text, Button, InBlockText, TextAlign, Input, TextSettings
 
 if TYPE_CHECKING:
     from src.app import App
@@ -24,48 +24,21 @@ class Intro(Scene):
         super().__init__(app)
 
     async def boot(self):
-        self.app.audio.load_sound('intro', os.path.join('assets', 'sounds', 'intro.wav'))
+        self.add_sprite('application_name', Text(self.app, Vector2(10, 10), 'Смотри Синхронно', 32,
+                                                 align=TextAlign.LEFT))
 
-        self.add_sprite('application_name', Text(self.app, Vector2(960, 540), 'Pygame Application', 48))
-        self.add_sprite('tip', Text(self.app, Vector2(960, 600), 'Press escape to quit'))
-
-        self.add_sprite('watching_the_rocket_button',
-                        Button(self.app, Vector2(555, 650), (400, 50),
-                               InBlockText(self.app, 'Watching the rocket'),
-                               self._on_watching_the_rocket_button_pressed))
-        self.add_sprite('connection_to_service_button',
-                        Button(self.app, Vector2(965, 650), (400, 50),
-                               InBlockText(self.app, 'Connection to service'),
-                               self._on_connection_to_service_button_pressed))
+        self.add_sprite('server_url', Input(self.app, Vector2(510, 505), (900, 70), TextSettings(self.app),
+                                            InBlockText(self.app, 'Адрес сервера', (128, 128, 128)),
+                                            default='127.0.0.1:36668'))
+        self.add_sprite('watching_the_rocket_button', Button(self.app, Vector2(760, 585), (400, 50),
+                                                             InBlockText(self.app, 'Подключиться с серверу')))
 
     async def update(self):
-        self._update_tip_color()
-
         if pg.key.get_pressed()[pg.K_ESCAPE]:
             self.app.quit()
 
     async def enter(self):
-        self.app.audio.play('intro')
-
-    def _update_tip_color(self):
-        """Implementation of text flickering.
-        """
-        tip: Text = self.get_sprite('tip')
-
-        color: tuple[int, int, int] = tuple[int, int, int]([int(155 - (sin(time.time() * 2) * 100))] * 3)
-
-        tip.color = color
-        tip.update_view()
-
-    async def _on_watching_the_rocket_button_pressed(self, context: Optional[str]):
-        """The handler for clicking on the button.
-        """
-        await self.app.change_scene('RocketGame')
-
-    async def _on_connection_to_service_button_pressed(self, context: Optional[str]):
-        """The handler for clicking on the button.
-        """
-        await self.app.change_scene('ConnectionToService')
+        pass
 
     async def exit(self):
         pass
