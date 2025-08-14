c8ce3a24c1e8be1179e683ba99235a398ae6241c
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-14 22:07:30 +0300

    Добавил загрузку

--- src/scenes/intro.py
+++ src/scenes/intro.py
@@ -10,7 +10,7 @@ from pygame import Vector2
 
 from src.scene import Scene
 
-from src.sprites import Text, Button, InBlockText, TextAlign, Input, TextSettings
+from src.sprites import Text, Button, InBlockText, TextAlign, Input, TextSettings, Waiting, CompletionStatus
 
 if TYPE_CHECKING:
     from src.app import App
@@ -30,12 +30,17 @@ class Intro(Scene):
         self.add_sprite('server_url', Input(self.app, Vector2(510, 505), (900, 70), TextSettings(self.app),
                                             InBlockText(self.app, 'Адрес сервера', (128, 128, 128)),
                                             default='127.0.0.1:36668'))
-        self.add_sprite('watching_the_rocket_button', Button(self.app, Vector2(760, 585), (400, 50),
-                                                             InBlockText(self.app, 'Подключиться с серверу')))
+        self.add_sprite('connect_button', Button(self.app, Vector2(760, 585), (400, 50),
+                                                 InBlockText(self.app, 'Подключиться с серверу'),
+                                                 self.on_connect_button_pressed))
+
+    async def on_connect_button_pressed(self, context: str):
+        self.remove_sprite('connect_button')
+        self.add_sprite('connect_waiting', Waiting(self.app, Vector2(760, 595), (400, 30),
+                                                   CompletionStatus.WORKING))
 
     async def update(self):
-        if pg.key.get_pressed()[pg.K_ESCAPE]:
-            self.app.quit()
+        pass
 
     async def enter(self):
         pass
