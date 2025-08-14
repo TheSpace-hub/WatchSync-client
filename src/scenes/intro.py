5f50905a7ce230ac71c68783ef03d83b63a879fb
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-14 22:56:54 +0300

    Пробуем подключение на вкус

--- src/scenes/intro.py
+++ src/scenes/intro.py
@@ -6,7 +6,6 @@ from asyncio import Task
 
 import ipaddress
 import aiohttp
-from aiohttp import ClientConnectorError
 from pygame import Vector2
 
 from src.scene import Scene
@@ -23,7 +22,7 @@ class Intro(Scene):
 
     def __init__(self, app: 'App'):
         super().__init__(app)
-        self.connection_task: Optional[Task] = None
+        self.taste_connection_task: Optional[Task] = None
 
     async def boot(self):
         self.add_sprite('application_name', Text(self.app, Vector2(10, 10), 'Смотри Синхронно', 32,
@@ -32,36 +31,37 @@ class Intro(Scene):
         self.add_sprite('server_url_input', Input(self.app, Vector2(510, 505), (900, 70), TextSettings(self.app),
                                                   InBlockText(self.app, 'Адрес сервера', (128, 128, 128)),
                                                   default='127.0.0.1'))
-        self.add_sprite('connect_button', Button(self.app, Vector2(760, 585), (400, 50),
-                                                 InBlockText(self.app, 'Подключиться с серверу'),
-                                                 self.on_connect_button_pressed))
+        self.add_sprite('taste_connection_button', Button(self.app, Vector2(760, 585), (400, 50),
+                                                          InBlockText(self.app, 'Подключиться с серверу'),
+                                                          self.on_taste_connection_button_pressed))
 
     async def update(self):
-        await self.update_task()
+        await self.update_taste_connection_task()
 
-    async def on_connect_button_pressed(self, context: str):
+    async def on_taste_connection_button_pressed(self, context: str):
         server_url_input: Input = self.get_sprite('server_url_input')
+        connect_button: Button = self.get_sprite('taste_connection_button')
+
         host = server_url_input.text.text
         if not self.is_valid_ip(host):
             return
 
-        connect_button: Button = self.get_sprite('connect_button')
         connect_button.disabled = True
         server_url_input.disabled = True
 
         self.add_sprite('connect_waiting', Waiting(self.app, Vector2(760, 645), (400, 30),
                                                    CompletionStatus.WORKING))
 
-        self.connection_task = asyncio.create_task(self.connect(f'http://{host}:22020/connect'))
+        self.taste_connection_task = asyncio.create_task(self.can_connect(host))
 
-    async def update_task(self):
-        """Update all the task responsible for the connection.
+    async def update_taste_connection_task(self):
+        """Update taste connection task.
         """
-        if self.connection_task and self.connection_task.done():
+        if self.taste_connection_task and self.taste_connection_task.done():
             waiting: Waiting = self.get_sprite('connect_waiting')
-            response: Optional[dict] = await self.connection_task
+            can_connect: bool = await self.taste_connection_task
 
-            if response is None:
+            if not can_connect:
                 server_url_input: Input = self.get_sprite('server_url_input')
                 connect_button: Button = self.get_sprite('connect_button')
 
@@ -70,25 +70,18 @@ class Intro(Scene):
                 server_url_input.disabled = False
                 return
 
-            waiting.completion_status = CompletionStatus.get_status_by_response_status_code(response['status'])
+            waiting.completion_status = CompletionStatus.SUCCESS
 
     @staticmethod
-    async def connect(url) -> Optional[dict]:
+    async def can_connect(host: str) -> bool:
         """Make a get request.
 
         Returns:
             Response data.
         """
         async with aiohttp.ClientSession() as session:
-            try:
-                async with session.get(url) as response:
-                    return {
-                        'status': response.status,
-                        'headers': response.headers,
-                        'body': await response.json()
-                    }
-            except ClientConnectorError:
-                return None
+            async with session.get(f'http://{host}:22020/taste') as response:
+                return (await response.json())['can_connect']
 
     @staticmethod
     def is_valid_ip(ip_str):
