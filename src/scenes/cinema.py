f33d6abf9d6e32ba15fe13ce75e1b434008648c8
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-14 22:36:38 +0300

    Добавил сцену cinema

---
+++ src/scenes/cinema.py
@@ -0,0 +1,30 @@
+"""A scene module with a cinema.
+"""
+from typing import TYPE_CHECKING, Optional
+from asyncio import Task
+
+from src.scene import Scene
+
+if TYPE_CHECKING:
+    from src.app import App
+
+
+class Intro(Scene):
+    """A class with a cinema.
+    """
+
+    def __init__(self, app: 'App'):
+        super().__init__(app)
+        self.connection_task: Optional[Task] = None
+
+    async def boot(self):
+        pass
+
+    async def update(self):
+        pass
+
+    async def enter(self):
+        pass
+
+    async def exit(self):
+        pass
